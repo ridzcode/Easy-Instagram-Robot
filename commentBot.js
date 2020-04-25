21219e9eb5d7520426b588aaa8543435e43fd169
Muhammad Ridzwan <56738676+ridz97@users.noreply.github.com>
2020-04-25 23:24:11 +0800

    Create commentBot.js

---
+++ commentBot.js
@@ -0,0 +1,4 @@
+// comment bot ---> under maintainance
+
+// let komen =  document.querySelector("textarea.Ypffh").value = "Test 123!!!"; // comment text area
+// let post = document.querySelector("button.y3zKF"); // getButton
