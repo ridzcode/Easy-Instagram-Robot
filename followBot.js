896d453014f0fec44f8f77377aa3c2644b45b709
Muhammad Ridzwan <56738676+ridzcode@users.noreply.github.com>
2020-12-02 23:15:59 +0800

    Update followBot.js

--- followBot.js
+++ followBot.js
@@ -1,5 +1,6 @@
 /** 
  * Instagram bot with web console
+ * Coded by @ridzcode
 */
 
 // Follow bot
@@ -9,5 +10,5 @@ for(var follow = 0; follow < 10; follow++) {
   // get button class
   document.getElementsByClassName("sqdOP")[follow].click();
   // console
-  console.log(`You followed ${follow + 1} account`);
+  console.log(`You followed ${follow + 1} account -- Coded by @ridzcode --`);
 }
